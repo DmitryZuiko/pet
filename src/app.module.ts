ffb8be6e662edec64f90020762f686a2a7e670b0
DmitryZuiko <zuiko.z@mail.ru>
2024-01-17 21:30:51 +0300

    move db connection in separate file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { DatabaseModule } from './db/database.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    UsersModule,
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        synchronize: true,
-        entities: [__dirname + '/**/*.entity{.js, .ts}'],
-      }),
-      inject: [ConfigService],
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), UserModule, DatabaseModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
